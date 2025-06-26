d1864420a14e880ddc5f37c80f6068f89910c2c2
KalykovRinat <97353822+KalykovRinat@users.noreply.github.com>
2025-06-26 20:41:15 +0600

    Delete handlers directory

--- handlers/is_admin.py
+++
@@ -1,7 +0,0 @@
-from aiogram.filters import BaseFilter
-from aiogram.types import Message
-from config import ADMIN_IDS
-
-class IsAdmin(BaseFilter):
-    async def __call__(self, message: Message) -> bool:
-        return message.from_user.id in ADMIN_IDS
