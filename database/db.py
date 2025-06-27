7796a12b159d0bac4afe20ebd1e7efcc85694dee
KalykovRinat <97353822+KalykovRinat@users.noreply.github.com>
2025-06-27 14:18:32 +0600

    Update db.py

--- database/db.py
+++ database/db.py
@@ -23,10 +23,12 @@ async def init_db_pool():
 async def get_accounts_by_category(category):
     async with db_pool.acquire() as conn:
         async with conn.cursor(aiomysql.DictCursor) as cur:
-            await cur.execute("SELECT * FROM accounts WHERE category=%s", (category,))
+            await cur.execute(
+                "SELECT * FROM accounts WHERE category = %s AND is_deleted = FALSE",
+                (category,)
+            )
             accounts = await cur.fetchall()
     return accounts
-
 
 async def insert_account(category, login, password, price_som,price_rub, description, binding, media_id=None, media_type=None):
     async with db_pool.acquire() as conn:
@@ -40,18 +42,25 @@ async def insert_account(category, login, password, price_som,price_rub, descrip
 async def get_all_accounts():
     async with db_pool.acquire() as conn:
         async with conn.cursor() as cur:
-            await cur.execute("SELECT id, category, login, price_som,price_rub FROM accounts")
+            await cur.execute(
+                "SELECT id, category, login, price_som, price_rub FROM accounts WHERE is_deleted = FALSE"
+            )
             rows = await cur.fetchall()
             return rows
+
 
 async def get_account_by_id(acc_id):
     async with db_pool.acquire() as conn:
         async with conn.cursor(aiomysql.DictCursor) as cur:
-            await cur.execute("SELECT * FROM accounts WHERE id = %s", (acc_id,))
+            await cur.execute(
+                "SELECT * FROM accounts WHERE id = %s AND is_deleted = FALSE",
+                (acc_id,)
+            )
             return await cur.fetchone()
+
 
 async def delete_account_by_id(acc_id):
     async with db_pool.acquire() as conn:
         async with conn.cursor() as cur:
-            await cur.execute("DELETE FROM accounts WHERE id = %s", (acc_id,))
+            await cur.execute("UPDATE accounts SET is_deleted = TRUE WHERE id = %s", (acc_id,))
         await conn.commit()
